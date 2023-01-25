f8c1be50c3a9a457fa63707b7c7176b0fdeb6710
DavidAshburn <oneinchwebbing@gmail.com>
2023-01-24 23:37:35 -0800

    refactored and added Circle to dragdrop

--- app/javascript/controllers/dragdrop_controller.js
+++ app/javascript/controllers/dragdrop_controller.js
@@ -10,20 +10,23 @@ static targets = [
     this.canvasTarget.height = Math.floor(window.innerHeight * .6);
   }
 
-  drawText() {
+  clear() {
+    let ctx = this.canvasTarget.getContext("2d");
+    ctx.beginPath();
+    ctx.clearRect(0,0,this.canvasTarget.width,this.canvasTarget.height);
+  }
+
+  georgeStGeegland() {
     let ctx = this.canvasTarget.getContext("2d");
     ctx.font="48px righteous";
     ctx.fillText("Oooooh, Hello", 10,50);
   }
 
-  driver() {
+  gillFaizan() {
     let width = this.canvasTarget.width;
     let height = this.canvasTarget.height;
-    //offset for distance from top of window to top of canvas, corrects mouse y coordinate
-    let cOffset = window.pageYOffset + this.canvasTarget.getBoundingClientRect().top;
     let ctx = this.canvasTarget.getContext("2d");
     let rect = this.canvasTarget.getBoundingClientRect();
-
     let mouse = {
       x: undefined,
       y: undefined
@@ -37,38 +40,51 @@ static targets = [
         if(mouse.y > rect.height)
           mouse.y = -1;
       })
-    
-    //divider x-coord
-    let barX = width * .8;
+    canvas.addEventListener('mousedown',
+      function(event) {
+        console.log("mousedown");
+      })
+    canvas.addEventListener('mouseup',
+      function(event) {
+        console.log("mouseup");
+      })
+    canvas.addEventListener('mouseout',
+      function(event) {
+        console.log("mouseout");
+      })
 
     //Drag and drop Objects
-    function Connection(x,y) {
+    function Circle(x,y,label,fill="#0891b2",stroke="#fcd34d") {
       this.x = x;
       this.y = y;
-
+      this.label = label;
+      this.radius = Math.min(height * .1, 30);
+      this.fill = fill;
+      this.stroke = stroke;
+      this.held = false;
       this.draw = function() {
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
+        ctx.strokeStyle = this.stroke;
+        ctx.fillStyle = this.fill;
+        ctx.stroke();
+        ctx.fill();
 
+        ctx.font = "24px righteous";
+        ctx.fillText(this.label, this.x + this.radius + 15, this.y + 5);
       }
-
       this.update = function() {
-
+        if(this.held) {
+          this.x = mouse.x;
+          this.y = mouse.y;
+        }
         this.draw();
       }
     }
 
-    function Rackroom(x,y) {
-      this.x = x;
-      this.y = y;
-
-      this.draw = function() {
-
-      }
-
-      this.update = function() {
-
-        this.draw();
-      }
-    }
+    //Prepping for animate function
+    //divider x-coord
+    let barX = width * .8;
 
     //set some ctx baselines
     ctx.font="48px righteous";
@@ -78,17 +94,21 @@ static targets = [
     let itemw = width * .2;
     let itemh = height * .2;
 
-    //menu script
+    //menu item script
     const bar = function(x,y,w,h) {
       ctx.fillRect(x, y, w, h)
       ctx.strokeRect(x, y, w, h)
     }
 
+    //Populate some stuff without drag and drop
+    let firstElement = new Circle(100,100,"Mainline");
+
     //our actual animate loop
     const animate = function() {
       requestAnimationFrame(animate);
       ctx.clearRect(0,0,width,height);
-
+      
+      ctx.strokeStyle = "#1c1917"
       //draw menu separator
       ctx.beginPath()
       ctx.moveTo(barX,0);
@@ -96,17 +116,20 @@ static targets = [
       ctx.stroke();
 
       //draw right-hand menu
-      ctx.fillStyle = "#777777";
+      ctx.fillStyle = "#3b82f6"
       for(let k = 0; k < 3; k++) {
         bar(barX, itemh * k, itemw, itemh)
       }
 
+      firstElement.update();
+
       //only draw the mousex,mousey if it's in the canvas area
-      if(mouse.x != undefined && mouse.x > -1 && mouse.y > -1) {
-        ctx.fillStyle = "#000000";
+      if(mouse.x > -1 && mouse.y > -1) {
         ctx.fillText(`${mouse.x},${mouse.y}`, Math.floor(width * .84), Math.floor(height * .9));
       }
     }
     animate();
   }
+
+
 }
